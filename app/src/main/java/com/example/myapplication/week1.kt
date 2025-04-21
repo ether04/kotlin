4dd2b7af8c583e5221a3332628c21a8209e7a543
bajra203! <sujalbajracharya2011@gmail.com>
2025-04-21 22:56:20 +0545

    sujal

---
+++ app/src/main/java/com/example/myapplication/week1.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+class week1 {
+    fun main(){
+        //mutable variable
+        var FirstName = "sujal"
+        var lastname = "project"
+
+        //string name = "sujal"
+
+        var name: string ="sujal"
+
+        val name : string = 10
+
+        var address = arrayListOf("ktm","chitwan")
+        address.add("pokhara")
+
+    }
+}
